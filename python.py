318759148214e3dccabfd259714e3420513b5a7b
minpurisathya <sathyaminpuri.com>
2024-07-09 14:02:28 +0530

    committing changes to local repo

---
+++ python.py
@@ -0,0 +1,8 @@
+# Function to perform arithmetic operations on two numbers
+def arithmetic_operations(num1, num2):
+    # Addition
+    addition = num1 + num2
+    print(f"Addition: {num1} + {num2} = {addition}")
+
+    # Subtraction
+    subtraction = num1
